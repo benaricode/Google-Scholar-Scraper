96705f60d9777a738f866a9defe1844152620197
Benjamin Lincoln <support@lincolnapps.net>
2017-06-04 11:22:33 +1000

    Added interface
    Added an interface to select the query and year #2 #3

--- functions.php
+++ functions.php
@@ -1,6 +1,15 @@
 <?php
 
-	searchScholar('Big Data', '2013');
+function get_or_default($arr, $key, $default) {
+	if(isset($arr[$key])) {
+		return $arr[$key];
+	} else {
+		return $default;
+	}
+}
+	if(isset($_POST['query'])){
+		searchScholar('Big Data', '2013');
+	}
 
 	function searchScholar($ThingToSearchFor, $year){
 
@@ -25,3 +34,46 @@
 		return str_replace(',', '', $output);
 	}
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Google Scholar Scaper</title>
+	</head>
+
+	<body>
+	<section>
+		<h1>Google Scholar Scaper and graph</h1>
+		<p>
+			This page exists as a way to quickly graph a year over year view of the use of a phrase in papers, this is useful to get a sense of when a field boomed
+
+			note, it may take awhile for the page to reload after clicking submit
+
+		</p>
+	</section>
+		<form action="functions.php" method="POST">
+			<ul>
+			<li>
+				<label for="Query">Query</label>
+				<input type="text" size="50" name="query" id="query">
+			</li>
+
+			<li>
+				<label for="year">Start Year</label>
+				<input type="number" size="10" name="year" id="year">
+			</li>
+			</ul>
+			<button>Search scholar</button>
+		</form>
+		<?php echo date("Y"); ?>
+
+		<?php if(isset($_POST['query'])){
+		
+		$query = get_or_default($_POST, 'query', '');
+		$startYear = get_or_default($_POST, 'year', '');
+		
+		echo searchScholar($query , $startYear);
+
+	}?>
+	</body>
+</html>
